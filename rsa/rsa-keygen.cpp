f8b734d800a0d1b362df72e8a36a5473cd4fc23a
Florian Thonig <florian.thonig96@gmail.com>
2019-06-30 23:12:01 +0200

    key pair generation for wiener attacks
    added option "--wiener" for wiener key pairs that match the condition for wiener attacks

--- rsa/rsa-keygen.cpp
+++ rsa/rsa-keygen.cpp
@@ -15,10 +15,18 @@
 using namespace Botan;
 
 int main(int argc, char** argv) {
+    bool gen_wiener = false;
     // need at least one argument with the keysize
     if (argc < 2) {
-        std::cerr << "Usage: rsa-keygen <keysize in bits>" << std::endl;
+        std::cerr << "Usage:\trsa-keygen <keysize in bits> [--wiener]" << std::endl;
+        std::cerr << "Note:\tthe --wiener option generates a key pair,\n\t\tthat matches the condition for a wiener attack" << std::endl;
         exit(-1);
+    }
+    if (argc > 2) {
+        if (strcmp(argv[2], "--wiener") == 0) {
+            gen_wiener = true;
+            std::cout << "[INFO] Generating a key pair for wiener attack" << std::endl;
+        }
     }
     uint64_t bitsize = strtoul(argv[1], nullptr, 10);
     // check for error or default bit size
@@ -38,7 +46,24 @@ int main(int argc, char** argv) {
         m_q = generate_rsa_prime_allow_small_keys(system_rng(), system_rng(), bits_q, exp);
         m_n = m_p * m_q;
     } while(m_n.bits() != bitsize);
-    RSA_PrivateKey privateKey (m_p, m_q, exp);
+    BigInt d;
+    BigInt phi = (m_p - 1) * (m_q - 1);
+    if (gen_wiener) {
+        do {
+            d = BigInt(system_rng(), sqrt(bits_p));
+        } while (d >= sqrt(sqrt(m_n))/3 || gcd(d, phi) != 1);
+    } else {
+        do {
+            d = BigInt(system_rng(), bits_p + sqrt(sqrt(bits_q)));
+        } while (gcd(d, phi) != 1);
+    }
+    BigInt e = inverse_mod(d, phi);
+    RSA_PrivateKey privateKey(m_p, m_q, e, d, m_n);
+    std::cout << "p: " << m_p << std::endl;
+    std::cout << "q: " << m_q << std::endl;
+    std::cout << "phi: " << phi << std::endl;
+    std::cout << "e: " << e << std::endl;
+    std::cout << "d: " << privateKey.get_d() << std::endl;
 
     std::ofstream pkFile("private.pem", std::ios::out | std::ios::trunc);
     if (!pkFile.is_open()) {
